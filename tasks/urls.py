620c51da4be97df62414865ac3d733c3f838f0df
Janzam <zambranoalejandra226@gmail.com>
2025-08-07 14:36:14 -0500

    edit

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import TaskViewSet
+
 router = routers.DefaultRouter()
 router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(router.urls)),      # Incluye las rutas de la API
+            # Ruta para la vista HTML
 ]
